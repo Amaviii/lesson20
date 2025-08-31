5f57fcdaeacf2d6e7c9cd6bcef8eed2ae998f460
Gregory <lubeevgs2000@gmail.com>
2025-08-31 21:41:51 +0300

    task-1 done

--- app/src/main/java/com/example/lesson20/MainActivity.kt
+++ app/src/main/java/com/example/lesson20/MainActivity.kt
@@ -5,11 +5,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.lesson20.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnMain?.setOnClickListener {
+            binding?.tvMain?.text = "Текст изменился"
+        }
     }
+
+
 }
